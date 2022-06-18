a7045b891077f0c3f5c8bdfe56e8cb275bf46d36
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-18 18:00:35 +0400

    June 18, 2022 6:00 PM

--- server.js
+++ server.js
@@ -21,11 +21,10 @@ io.on('connection', socket => {
     // socket.to(roomId).broadcast.emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      // socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
 })
 
 server.listen(3000)
-
 
