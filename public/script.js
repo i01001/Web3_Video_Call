1ae977a76517ba7c24f6005b4aca141a52f5875f
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-19 02:35:33 +0400

    June 19, 2022 2:35 AM

--- public/script.js
+++ public/script.js
@@ -23,8 +23,9 @@ const uauth = new UAuth({
   // clientID: 'uauth_example_spa_id',
   // redirectUri: 'http://localhost:5000/callback',
     clientID: "aa3d2d7a-38ee-49a4-89ba-a6fe5398e570",
-    redirectUri: "http://localhost/3000",
-    scope: "openid wallet email:optional humanity_check:optional",
+    redirectUri: "http://localhost:3000",
+    scope: "openid wallet email:optional humanity_check:optional"
+
 })
 
 
