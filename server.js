7fb53825659ce8a785886867de41dea9dbbe064f
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-18 19:49:11 +0400

    June 18, 2022 7:49 PM

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
+var ExpressPeerServer = require("peer").ExpressPeerServer;    
+var options = {
+  debug: true,
+  allow_discovery: true,
+};
+let peerServer = ExpressPeerServer(server, options);
+app.use("/peerjs", peerServer);
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
